6ea53bd3b7c24f1b5f0faf36bf3e5ee2e7d43197
baktiyar <dushebaev.baktiyar@gmail.com>
2019-02-27 09:37:21 +0600

    Add new Home component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
